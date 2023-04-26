0799caadf0292819f742277c8282eb30b58e4d33
Sribastav Rajguru <sribastavrajguru@analytic-edge.com>
2023-04-26 18:52:32 +0530

    Feature:modal: Added keypress event to close modal

--- modal/script.js
+++ modal/script.js
@@ -19,3 +19,8 @@ for (let btn of showModalButtons) {
 }
 closeModalButton.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
+    modal.classList.add("hidden");
+  }
+})
